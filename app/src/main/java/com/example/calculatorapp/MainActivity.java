929e15cf29575085e6cb0c9ae03b214195b66bc7
Eric Zhan <zhan1304@students.d211.org>
2022-08-19 12:54:54 -0500

    Adding operators

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -14,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void operation()
+    {
+
+    }
+
     public void findSum(View view) {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
@@ -24,6 +30,18 @@ public class MainActivity extends AppCompatActivity {
         int sum = num1 + num2;
 
         numberSumTV.setText("" + sum);
+    }
+
+    public void findDifference(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt((number1ET.getText().toString()));
+        int num2 = Integer.parseInt((number2ET.getText().toString()));
+        int diff = num1 - num2;
+
+        numberSumTV.setText("" + diff);
     }
 
 }
