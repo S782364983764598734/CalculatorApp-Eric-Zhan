2c94bcde4bd7010d2a875b01813fcd6486872820
Eric Zhan <mightyerwa@gmail.com>
2022-08-23 13:13:36 -0500

    Adding Spinner
    Almost done calling spinner to on click

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,34 +3,36 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     String[] operators = {"+","-","*","/"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Spinner op =  (Spinner)findViewById(R.id.operator);
+        //initializes Spinner class in order for users to select the operator that they want
+        Spinner op = (Spinner)findViewById(R.id.operator);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.operator, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        op.setAdapter(adapter);
         op.setOnItemSelectedListener(this);
-
-        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item, operators);
-
-        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        op.setAdapter(ad);
+        @Override
+            public void operation(View view)
+            {
+                String current = op.getSelectedItem().toString();
+            }
     }
 
-    public void operation()
-    {
+    //this class determines which operator to use, depending on what operator the user chooses
 
-    }
 
     public void findSum(View view) {
         EditText number1ET = findViewById(R.id.num1ET);
@@ -56,4 +58,14 @@ public class MainActivity extends AppCompatActivity {
         numberSumTV.setText("" + diff);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
